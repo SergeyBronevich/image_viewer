341e81280af507e8ae463dccb7a15b5e23d9d1e6
SergeyBronevich <sergey.bronevich@gmail.com>
2019-08-08 10:16:22 +0400

    Updates.

--- src/image_viewer.hpp
+++ src/image_viewer.hpp
@@ -4,13 +4,13 @@
 
 namespace image_viewer
 {
-	// Returns list of image paths in the directory
+    // Returns list of image paths in the directory
     std::vector<cv::String> list_images(const char* directoryPath);
 
     static const int DEFAULT_HISTOGRAM_WIDTH = 256;
     static const int DEFAULT_HISTOGRAM_HEIGHT = 256;
 
-	// Makes histogram image
+    // Makes histogram image
     cv::Mat make_histogram_image(
         const cv::Mat& image, int histogramWidth = DEFAULT_HISTOGRAM_WIDTH,
         int histogramHeight = DEFAULT_HISTOGRAM_HEIGHT);
